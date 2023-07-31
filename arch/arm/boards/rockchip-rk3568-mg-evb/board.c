cc7d369070b48c3c386e7f16b5de76f5effb36af
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-07-31 17:20:26 +0300

    Avoid syslog flood if ES8388 is missing

--- arch/arm/boards/rockchip-rk3568-mg-evb/board.c
+++ arch/arm/boards/rockchip-rk3568-mg-evb/board.c
@@ -7,9 +7,52 @@
 #include <environment.h>
 #include <envfs.h>
 #include <init.h>
+#include <i2c/i2c.h>
 #include <mach/rockchip/bbu.h>
 
-static int rk3568_mg_evb_probe(struct device *dev)
+static void rk3568_mg_evb_disable_device(struct device_node *root,
+					 const char *label)
+{
+	struct device_node *np = of_find_node_by_name(root, label);
+	if (np)
+		of_device_disable(np);
+}
+
+static int rk3568_mg_evb_probe_i2c(struct i2c_adapter *adapter, int addr)
+{
+	struct i2c_client client;
+	u8 value;
+
+	client.adapter = adapter;
+	client.addr = addr;
+
+	return (i2c_read_reg(&client, 0, &value, 1) < 0) ? -ENODEV : value;
+}
+
+static int rk3568_mg_evb_i2c_fixup(struct device_node *root, void *unused)
+{
+	struct i2c_adapter *adapter = i2c_get_adapter(4);
+	if (!adapter)
+		return -ENODEV;
+
+	if (rk3568_mg_evb_probe_i2c(adapter, 0x10) < 0) {
+		pr_warn("ES8388 not found, disabling sound node.\n");
+		rk3568_mg_evb_disable_device(root, "sound");
+	}
+
+	return 0;
+}
+
+static __init int rk3568_mg_evb_fixup(void)
+{
+	if (of_machine_is_compatible("macro,rk3568-evb"))
+		of_register_fixup(rk3568_mg_evb_i2c_fixup, NULL);
+
+	return 0;
+}
+postcore_initcall(rk3568_mg_evb_fixup);
+
+static int __init rk3568_mg_evb_probe(struct device *dev)
 {
 	enum bootsource bootsource = bootsource_get();
 	int instance = bootsource_get_instance();
