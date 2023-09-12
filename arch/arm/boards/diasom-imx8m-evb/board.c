8f1c6bb066ae6cdcd92caf2f4c5f1f4066f99cd6
Alexander Shiyan <eagle.alexander923@gmail.com>
2023-09-12 09:07:52 +0300

    imx8m: Prepare to use i2c hooks

--- arch/arm/boards/diasom-imx8m-evb/board.c
+++ arch/arm/boards/diasom-imx8m-evb/board.c
@@ -2,10 +2,45 @@
 
 #include <bootsource.h>
 #include <common.h>
+#include <deep-probe.h>
 #include <envfs.h>
 #include <init.h>
-#include <deep-probe.h>
+#include <i2c/i2c.h>
 #include <mach/imx/bbu.h>
+
+#if 0
+static void diasom_imx8m_evb_disable_device(struct device_node *root,
+					    const char *label)
+{
+	struct device_node *np = of_find_node_by_name(root, label);
+	if (np)
+		of_device_disable(np);
+}
+
+static int diasom_imx8m_evb_probe_i2c(struct i2c_adapter *adapter, const int addr)
+{
+	u8 buf[1];
+	struct i2c_msg msg = {
+		.addr = addr,
+		.buf = buf,
+		.len = sizeof(buf),
+		.flags = I2C_M_RD,
+	};
+
+	return (i2c_transfer(adapter, &msg, 1) == 1) ? 0: -ENODEV;
+}
+#endif
+
+static int diasom_imx8m_evb_fixup(struct device_node *root, void *unused)
+{
+	struct i2c_adapter *adapter = i2c_get_adapter(2);
+	if (!adapter)
+		return -ENODEV;
+
+	//TODO:
+
+	return 0;
+}
 
 static int diasom_imx8m_evb_probe(struct device *dev)
 {
@@ -31,6 +66,8 @@ static int diasom_imx8m_evb_probe(struct device *dev)
 	imx8m_bbu_internal_mmcboot_register_handler("eMMC", "/dev/mmc2", flag);
 
 	defaultenv_append_directory(defaultenv_diasom_imx8m_evb);
+
+	of_register_fixup(diasom_imx8m_evb_fixup, NULL);
 
 	return 0;
 }
